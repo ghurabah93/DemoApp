e88a0d83e779d5f238e86310e0dd8e1c87976e3d
Musa <ghurabah93@hotmail.com>
2017-05-01 15:01:59 +0800

    TextFields

--- app/src/main/java/com/robpercival/demoapp/MainActivity.java
+++ app/src/main/java/com/robpercival/demoapp/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
-        Log.i("Info", "Button Pressed!");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        EditText myPasswordField = (EditText) findViewById(R.id.myPasswordField);
+
+        Log.i("Info", "Your username is : " + myTextField.getText().toString() + " and your password is " + myPasswordField.getText().toString());
     }
 
     @Override
